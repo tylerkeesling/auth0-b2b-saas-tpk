f269c486b32c07e2c92982dee4e20f9c2f09e856
Tyler Keesling <tyler.keesling@gmail.com>
2025-04-11 20:37:39 -0400

    prefetching on the sidebar nav component

--- components/sidebar-nav.tsx
+++ components/sidebar-nav.tsx
@@ -27,6 +27,7 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
     >
       {items.map((item) => (
         <Link
+          prefetch={true}
           key={item.href}
           href={item.href}
           className={cn(
