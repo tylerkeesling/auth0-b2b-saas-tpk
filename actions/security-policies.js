4539a6adf59ac4ba1394fc273284dc50d59eda24
Tyler Keesling <tyler.keesling@gmail.com>
2026-02-13 20:15:15 -0500

    security policies update

--- actions/security-policies.js
+++ actions/security-policies.js
@@ -6,31 +6,51 @@
  */
 exports.onExecutePostLogin = async (event, api) => {
   if (event.client.client_id !== event.secrets.DASHBOARD_CLIENT_ID) return
+  if (event?.transaction?.protocol === "oauth2-refresh-token") return
 
   const mfaPolicy = JSON.parse(event.organization?.metadata.mfaPolicy || "{}")
 
   if (mfaPolicy.enforce) {
+    let isUsingPasskey = false
+    let isUsingFederation = false
+
+    for (const method of event?.authentication?.methods || []) {
+      if (method.name === "passkey") isUsingPasskey = true
+      if (method.name === "federated") isUsingFederation = true
+    }
+
+    /* Skip MFA if using Passkeys */
+    if (mfaPolicy.skipForPasskey && isUsingPasskey) return
+    /* Skip MFA if using SSO / federation */
+    if (mfaPolicy.skipForFederation && isUsingFederation) return
+
     // user is not enrolled in any factors but MFA is enforced
     if (!event.user.multifactor || event.user.multifactor.length === 0) {
-      return api.authentication.enrollWithAny(mfaPolicy.providers.map((p) => ({ type: p })))
+      return api.authentication.enrollWithAny(
+        mfaPolicy.providers.map((p) => ({ type: p }))
+      )
     }
 
     if (mfaPolicy.skipForDomains.length > 0) {
       // ensure a reasonable format for the email
-      const splitEMail = (event.user.email || "").split('@');
+      const splitEMail = (event.user.email || "").split("@")
       if (splitEMail.length !== 2) {
-        return api.access.deny('Email is invalid');
+        return api.access.deny("Email is invalid")
       }
 
-      const domain = splitEMail[1].toLowerCase();
+      const domain = splitEMail[1].toLowerCase()
 
       // if the MFA policy is set to enforce and the domain is not part
       // of the list of domains to handle MFA externally at the IdP
       if (!mfaPolicy.skipForDomains.includes(domain)) {
-        api.authentication.challengeWithAny(mfaPolicy.providers.map((p) => ({ type: p })))
+        api.authentication.challengeWithAny(
+          mfaPolicy.providers.map((p) => ({ type: p }))
+        )
       }
     } else {
-      api.authentication.challengeWithAny(mfaPolicy.providers.map((p) => ({ type: p })))
+      api.authentication.challengeWithAny(
+        mfaPolicy.providers.map((p) => ({ type: p }))
+      )
     }
   }
-};
+}
