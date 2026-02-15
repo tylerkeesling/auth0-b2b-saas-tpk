f16e389ec0c0f72a6fa44f1ccad978d01f7cdb0d
Tyler Keesling <tyler.keesling@gmail.com>
2026-02-15 16:35:48 -0500

    handle 404

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
+
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle>Page Not Found</CardTitle>
+          <CardDescription>
+            The page you&apos;re looking for doesn&apos;t exist or has been
+            moved.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Link href="/" className="w-full">
+            <Button className="w-full">
+              Go to Homepage <ArrowRightIcon className="ml-2 size-4" />
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
